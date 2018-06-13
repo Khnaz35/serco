3fdf610eddacf6efc1becf4e9c2a8b0c3d2c6808
Alexandr <dmytryk.alexandr@gmail.com>
2018-06-14 01:46:38 +0300

    Latest products page

---
+++ catalog/language/ru-ru/product/latest.php
@@ -0,0 +1,31 @@
+<?php
+$_['heading_title']     = 'Новинки';
+$_['meta_title']        = 'meta_titleНовинки';
+$_['meta_description']  = 'meta_descriptionНовинки';
+$_['meta_keyword']      = 'meta_keywordНовинки';
+
+// Text
+$_['text_refine']       = 'Уточнить поиск';
+$_['text_product']      = 'Товары';
+$_['text_error']        = 'Категория не найдена!';
+$_['text_empty']        = 'В этой категории нет товаров.';
+$_['text_quantity']     = 'Кол-во:';
+$_['text_manufacturer'] = 'Производитель:';
+$_['text_model']        = 'Код товара:';
+$_['text_points']       = 'Бонусные баллы:';
+$_['text_price']        = 'Цена:';
+$_['text_tax']          = 'Без налога:';
+$_['text_compare']      = 'Сравнение товаров (%s)';
+$_['text_sort']         = 'Сортировать по:';
+$_['text_default']      = 'По умолчанию';
+$_['text_name_asc']     = 'По имени (A - Я)';
+$_['text_name_desc']    = 'По имени (Я - A)';
+$_['text_price_asc']    = 'Цене (возрастанию)';
+$_['text_price_desc']   = 'Цене (убыванию)';
+$_['text_rating_asc']   = 'Популярности (возрастанию)';
+$_['text_rating_desc']  = 'Популярности (убыванию)';
+$_['text_model_asc']    = 'По модели (A - Я)';
+$_['text_model_desc']   = 'По модели (Я - A)';
+$_['text_code_asc']     = 'Номеру (возрастанию)';
+$_['text_code_desc']    = 'Номеру (убыванию)';
+$_['text_limit']        = 'Показывать:';
